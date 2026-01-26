f8a7139e37b54557072114e5c35e5120f60ad2bd
KobusDev <fafastforward@gmail.com>
2026-01-26 21:52:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const db = new sqlite3.Database("./users.db");
 db.run(`
 CREATE TABLE IF NOT EXISTS users (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username TEXT UNIQUE,
-  password TEXT
+  Home,
+  Kobus@jvr
 )
 `);
 
